09fd3cea07d9f1dd7195b08331df4818878225d4
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-16 00:07:36 +0200

    Profile App data

---
+++ src/data.js/profile-data.js
@@ -0,0 +1,20 @@
+export const profiles = [
+    {
+        img: 'https://lncimg.lance.com.br/cdn-cgi/image/width=1214,height=560,quality=75,format=webp/uploads/2024/03/AFP__20240304__2056874188__v3__HighRes__GoldenStateWarriorsVBostonCeltics-scaled-aspect-ratio-512-320.jpg',
+        name: 'Steph CURRY',
+        job: 'Basket ball Professional Player',
+        link: 'https://x.com/StephenCurry30'
+    },
+    {
+        img: 'https://lncimg.lance.com.br/cdn-cgi/image/width=1214,height=560,quality=75,format=webp/uploads/2024/03/AFP__20240304__2056874188__v3__HighRes__GoldenStateWarriorsVBostonCeltics-scaled-aspect-ratio-512-320.jpg',
+        name: 'Harry MacCode',
+        job: 'Software Engineer',
+        link: 'https://x.com/StephenCurry30'
+    },
+    {
+        img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcTh_SqmIsaWu48bCCj1iQGECWPhv7sIpdjmPng4gJcZuZn1L2329EncHWls6MygqgURCmocCOgZJQ_vB_4',
+        name: 'Pierre-Emerick Aubameyang',
+        job: 'Football Professional Player',
+        link: 'https://x.com/StephenCurry30'
+    },
+]
